f92292e6c2c39f2570e608bda2fa3264498b7c1b
Ultime 5528 <keranna.first@gmail.com>
2018-10-15 16:44:03 -0400

    Début tâche BasePilotable

---
+++ src/main/java/frc/robot/K.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * determiner les ports et variables.
+ */
+public class K {
+
+  public static class Ports {
+
+    public static final int BASE_PILOTABLE_MOTEUR_DROIT = 0;
+    public static final int BASE_PILOTABLE_MOTEUR_GAUCHE = 1;
+  
+
+  }
+
+  public static class BasePilotable {
+
+    
+  }
+
+  
+
+
+
+}
